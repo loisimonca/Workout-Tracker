45e225cfd790ed65b9d118ea5e12237d7f70bc2d
Lois Simonca <loisgabor@gmail.com>
2021-01-04 23:31:00 -0500

    added to the models folder, workout.js

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    enum: ["Resistance", "Cardio"],
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for the workout",
+    unique: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: [1],
+    trim: true,
+  },
+  weight: {
+    type: Number,
+    required: "Enter desired weight",
+    min: [1],
+    trim: true,
+  },
+  set: {
+    type: Number,
+    required: true,
+    min: [1],
+    trim: true,
+  },
+  reps: {
+    type: Number,
+    required: true,
+    min: [1],
+    trim: true,
+  },
+  distance: {
+    type: Number,
+    required: true,
+    min: [0.25],
+    trim: true,
+  },
+});
+
+const Transaction = mongoose.model("Workout", workoutSchema);
+
+module.exports = Transaction;
